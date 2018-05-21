f80d29d4d249dffbf9757503f62980c342d9a811
Yury Panyukov <yury@easyhire.me>
2018-05-21 14:48:15 +0300

    Added cache model and controller

---
+++ server/controllers/cache.js
@@ -0,0 +1,98 @@
+const Cache = require('../models/cache');
+
+const Logger = require('../../initializers/logger');
+
+module.exports = (app) => {
+  app.get('/cache', async (req, res, next) => {
+    try {
+      const result = await Cache.find();
+
+      res.status(200).json(result);
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  app.post('/cache', async (req, res, next) => {
+    const newCacheObject = new Cache(req.body);
+
+    try {
+      await newCacheObject.save();
+
+      res.status(200).json(newCacheObject);
+    } catch (e) {
+      if (e.name === 'ValidationError') e.statusCode = 400; // bad request
+
+      next(e);
+    }
+  });
+
+  app.get('/cache/:key', async (req, res, next) => {
+    const { key } = req.params;
+
+    try {
+      const cacheObject = await Cache.findByKey(key);
+
+      if (cacheObject && !cacheObject.ttlExceeded) {
+        Logger.info('Cache hit');
+
+        res.status(200).json(cacheObject);
+      } else {
+        Logger.info('Cache miss');
+
+        const newCacheObject = await Cache.findOneAndUpdate({ key }, {
+          key,
+          ttl: cacheObject && cacheObject.ttl,
+          rnd: Date.now(),
+        }, {
+          upsert: true,
+          new: true,
+        });
+
+        res.status(200).send(newCacheObject.rnd);
+      }
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  app.put('/cache/:key', async (req, res, next) => {
+    const { key } = req.params;
+    const updateData = req.body;
+
+    try {
+      const cacheObject = await Cache.findByKey(key);
+      if (cacheObject) {
+        Object.assign(cacheObject, updateData);
+
+        await cacheObject.validate();
+        await cacheObject.save();
+
+        res.status(200).json(cacheObject);
+      } else {
+        res.status(404).send(`Not found Cache item by key '${key}'`);
+      }
+    } catch (e) {
+      if (e.name === 'ValidationError') e.statusCode = 400; // bad request
+
+      next(e);
+    }
+  });
+
+  app.delete('/cache/:key', async (req, res, next) => {
+    const { key } = req.params;
+
+    try {
+      const cacheObject = await Cache.findByKey(key);
+      if (cacheObject) {
+        await cacheObject.remove();
+
+        res.status(200).send('removed');
+      } else {
+        res.status(404).send(`Not found Cache item by key '${key}'`);
+      }
+    } catch (e) {
+      next(e);
+    }
+  });
+};
---
+++ server/models/cache.js
@@ -0,0 +1,61 @@
+const config = require('config');
+const mongoose = require('mongoose');
+const mongooseUniqueValidator = require('mongoose-unique-validator');
+
+const { Schema } = mongoose;
+
+const Cache = new Schema({
+  key: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+  },
+
+  rnd: {
+    type: String,
+    default: Date.now(),
+  },
+
+  // milliseconds
+  ttl: {
+    type: Number,
+    default: 0,
+  },
+}, {
+  timestamps: true,
+});
+
+Cache.set('toJSON', {
+  virtuals: true,
+});
+
+Cache.post('save', function checkCountBeforeSave() {
+  this.model('Cache').checkCount();
+});
+
+Cache.statics.checkCount = async function checkCount() {
+  const countOfItems = await this.count();
+  const maxCacheEntries = config.get('maxCacheEntries');
+
+  if (countOfItems > maxCacheEntries) {
+    const removeItems = await this.find().sort('-createdAt').limit(countOfItems - maxCacheEntries);
+    await this.remove({ _id: removeItems.map(i => i._id) });
+  }
+};
+
+Cache.statics.findByKey = async function findByKey(key) {
+  const result = await this.findOne({ key });
+
+  this.checkCount();
+
+  return result;
+};
+
+Cache.virtual('ttlExceeded').get(function ttlExceeded() {
+  return Date.now() - this.ttl > new Date(this.createdAt).getTime();
+});
+
+Cache.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.model('Cache', Cache);
